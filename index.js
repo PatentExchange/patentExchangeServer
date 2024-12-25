98a3d7aeb736134ba37806ee52bfd70ccd10f1f0
goutham <goutham.msd7@gmail.com>
2024-12-26 02:59:23 +0530

    JWT implemented for user model (login/sign up)

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const argon2 = require("argon2")
 const UserModel = require("./models/Users")
+const jwt = require("jsonwebtoken")
 require("dotenv").config()
 
 const app = express()
@@ -20,19 +21,28 @@ app.post("/signup",(req,res)=>{
     UserModel.create(req.body).then(u=>res.json(u)).catch(e=>res.json(e))
 })
 
-app.post("/login",(req,res)=>{
-    const uf= UserModel.findOne({email:req.body.email});
+app.post("/login",async (req,res)=>{
+    const uf= await UserModel.findOne({email:req.body.email});
     if(uf){
-        const isPasswordValid  = argon2.verify(uf.password,req.body.password);
+        const isPasswordValid  = await argon2.verify(uf.password,req.body.password);
         if (!isPasswordValid) {
             return res.status(400).json({ message: "Invalid password." });
         }
-
-        res.status(200).json({ message: "Login successful!" });
+        const token = jwt.sign({id: uf._id,name: uf.name,email:uf.email},process.env.JWT_SECRET)
+        res.status(200).json({ message: "Login successful!" ,token});
     }else{
-        console.error(error);
+        
         res.status(500).json({ message: "An error occurred during login.", error });
     }
+})
+
+app.get("/get-user/:id",async(req,res)=>{
+    const id = req.params.id;
+    const uf = await UserModel.findById({_id:id}).then((user)=>{
+        res.json(user);
+        console.log(user)
+    }).catch(err=>res.json(err));
+
 })
 
 app.listen(3002,()=>{console.log("Server has started")})
