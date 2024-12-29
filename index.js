1d00eeaa539991c7ced92122878000c6f57fb8b8
PatentExchange <exchangepatent@gmail.com>
2024-12-29 17:25:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ require("dotenv").config()
 
 const app = express()
 const corsOptions = {
-    origin: 'https://patentexchangeserver.onrender.com', // Allow requests from this origin
-    methods: 'GET,POST,PUT,DELETE', // Allow specific HTTP methods
-    credentials: true, // Allow cookies and credentials
+    origin: 'https://patentexchangeserver.onrender.com',
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
   };
   
 app.use(cors(corsOptions));
@@ -20,10 +20,6 @@ app.use(express.json())
 app.use(cors())
 
 try{
-    mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
     mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
