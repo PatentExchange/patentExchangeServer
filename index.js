a58f03bc0d9a7be065f73507a3876dca57485171
PatentExchange <exchangepatent@gmail.com>
2024-12-29 17:10:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ app.use(express.json())
 app.use(cors())
 
 try{
-    mongoose.connect("mongodb://localhost:27017/patentExchange")
+    mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
     console.log("connected to mongodb database")
 }catch(error){
     console.log("error connecting to database"+ error)
